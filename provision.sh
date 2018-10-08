97d6673112124d546b6fdf9d380bd2387b2c3304
Fatih I <fatihi@Fatihs-MacBook-Pro.local>
2018-10-08 13:24:55 +0100

    Clean brew packages

--- provision.sh
+++ provision.sh
@@ -5,60 +5,27 @@ set -o pipefail
 
 user=$(id -un)
 
-#
-# Execute commands as the superuser
-#
-sudo --validate
+install_brew_packages()
+{
+    echo "Brew updating"
+    brew update
+    brew bundle --file=modules/Brewfile
+    brew doctor
+    echo "Brew update completed"
+}
 
 #
 # Install brew
 #
 which -s brew
-if [[ $? != 0 ]] ; then
+if [[ $? == 1 ]] ; then
 	echo "Homebrew not installed, installing now..."
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	brew install caskroom/cask/brew-cask
+	install_brew_packages
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-echo "Brew updating"
-brew update
-brew bundle --file=modules/Brewfile
-brew doctor
-echo "Brew update completed"
-
-#
-# Create symbolic links to Git config files
-#
-gitconfig="/Users/${user}/.gitconfig"
-if [ -e "$gitconfig" ]
-then
-	echo "$gitconfig found. Not Creating"
 else
-    ln ./modules/git/.gitconfig ~/.gitconfig
-    ln ./modules/git/.gitignore-global ~/.gitignore-global
+    install_brew_packages
 fi
 
-
-#
-# Install Git pair program (TEAM WORKSTATION ONLY)
-#
-#gem install pivotal_git_scripts
-#echo "Git pair installed"
-#ln ./modules/git/.pairs ~/.pairs
-
-#
-# Start docker containers
-#
-#echo "Starting docker containers"
-#if [[ $(docker ps -a -q) == 0 ]] ; then
-#    docker kill $(docker ps -a -q)
-#fi
-#docker-compose -f modules/docker-compose.yml up &
-
-#
-# Set Zsh as default shell
-#
-#sudo echo "$(which zsh)" >> /etc/shells
-chsh -s $(which zsh)
-zsh -f
+echo "finished"
